f6ce7208f4cc661ee0402efcba8e5c0a55a585ec
MaxSavostyanov <maxsavostyanov34@gmail.com>
2022-08-20 20:01:28 +0300

    Update block popup

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,9 @@ import { initialCards } from './initialCards.js';
 const btnEdit = document.querySelector('.profile__btn_type_edit');
 const btnAdd = document.querySelector('.profile__btn_type_add');
 const cardsList = document.querySelector('.cards__list');
+const popup = document.querySelector('.popup');
+const popupContainer = popup.querySelector('.popup__container');
+const btnClosePopup = popup.querySelector('.popup__btn_type_close');
 
 function createCard (name, link) {
   const cardTemplate = document.querySelector('#card-template').content;
@@ -26,9 +29,28 @@ function createCard (name, link) {
   cardsList.append(cardElement);
 }
 
+function renderInitialCards (arrCards) {
+  arrCards.forEach((item) => createCard(item.name, item.link));
+}
+
 function toggleLike(btnLike) {
   btnLike.classList.toggle('card__btn-like_actived');
   btnLike.title = btnLike.classList.contains('card__btn-like_actived') ?'Убрать лайк' : 'Поставить лайк';
 }
 
-initialCards.forEach((item) => createCard(item.name, item.link));
+function openPopup (typeContent) {
+  popup.classList.remove('popup_closed');
+  popup.classList.add('popup_opened');
+}
+
+function closePopup () {
+  popup.classList.remove('popup_opened');
+  popup.classList.add('popup_closed');
+}
+
+btnEdit.addEventListener('click', () => openPopup());
+btnAdd.addEventListener('click', () => openPopup());
+
+btnClosePopup.addEventListener('click', () => closePopup());
+
+renderInitialCards (initialCards);
